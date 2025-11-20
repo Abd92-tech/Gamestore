850b14b5f8b39396eeaddc84ce692d9b5aedc988
Mehdi92-tech <mehdi070408@gmail.com>
2025-11-20 17:38:21 +0100

    Create API.js

---
+++ dev/API.js
@@ -0,0 +1,18 @@
+const url = https://api.rawg.io/api/platforms?key=890a02b254564eae8e3dda102a5ea1f3
+
+fetch(url)
+
+
+    .then(reponse => reponse.json())
+
+
+    .then(data => {
+
+
+        console.log("Données Jeux :");
+
+
+        console.log(data =>
+
+
+    })ssssssssssssssss
