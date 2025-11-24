fbff811210b012e48fa7e1cfb3ea7db8679e6afe
Mehdi92-tech <mehdi070408@gmail.com>
2025-11-24 16:47:14 +0100

    Update API.js

--- dev/API.js
+++ dev/API.js
@@ -1,6 +1,11 @@
+<<<<<<< Updated upstream
 const url = https//api.rawg.io/api/platforms?key=890a02b254564eae8e3dda102a5ea1f3
 
 fetch={url}
+=======
+const url = "https://api.rawg.io/api/platforms?key=890a02b254564eae8e3dda102a5ea1f3"
+fetch(url)
+>>>>>>> Stashed changes
 
 
     .then(reponse => reponse.json())
@@ -9,10 +14,16 @@ fetch={url}
     .then(data => {
 
 
-        console.log("Données Jeux :");
+        console.log("Données météo :");
 
 
+<<<<<<< Updated upstream
         console.log("data:")
  
 
+=======
+        console.log(data);
+
+
+>>>>>>> Stashed changes
     })
